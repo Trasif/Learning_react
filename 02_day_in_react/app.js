fdd4e40e847e7f5a9c732cb64ea086390de8ceb4
Asif raza <130272881+Trasif@users.noreply.github.com>
2023-12-15 11:03:23 +0000

    Created app.js and index.html file in second 02_day_in_react

---
+++ 02_day_in_react/app.js
@@ -0,0 +1,20 @@
+
+const heading = React.createElement(
+    "h1",
+    { id: "lal" }, // Here we write the attribute of element
+    "This is heading inside reach"
+  );
+  
+  const display = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "THis is h1 inside the child"),
+      React.createElement("h2", {}, "This is h2 inside child"),
+    ])
+  );
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(display);
+  
