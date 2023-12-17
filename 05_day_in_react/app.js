1eb07c937f7648291d3090a8f663a1fae339ebb0
Asif raza <130272881+Trasif@users.noreply.github.com>
2023-12-17 09:14:21 +0000

    yaar sirf error aa rhae hai

--- 05_day_in_react/app.js
+++ 05_day_in_react/app.js
@@ -1,3 +1,14 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from 'reactdom';
 
+// React component 
+// Functional React component
+const Heading = () => {
+    return <h1>This is a React component</h1>;
+}
+
+// Find the root element by its ID
+const rootElement = document.getElementById("root");
+
+// Render the Heading component to the root element
+rootElement.render(<Heading/>)
